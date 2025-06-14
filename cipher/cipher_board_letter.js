fbc95d9d893cca2241702c2042ad91b9369a331e
Danny Engelman <danny@engelman.nl>
2025-06-14 17:30:19 +0200

    v5 bugfixes

--- cipher/cipher_board_letter.js
+++ cipher/cipher_board_letter.js
@@ -41,11 +41,13 @@
                     // ==================================================== connectedCallback
                     // The connectedCallback is a default Web Components callback, triggered when the HTML tag is created in the DOM
                     connectedCallback() {
+                        // requestAnimationFrame(() => {
                         super.connectedCallback(); // call <chessmeister-board>.connectedCallback
                         // ----------------------------------------------------
                         if (this.hasAttribute("showletter")) this.showletter();
                         // ----------------------------------------------------
                         this.part = "letter"; // make global CSS styling possible
+                        // })
                     }
                     // ==================================================== get/set fen
                     // overload <chessmeister-board> get/set fen to add a bit extra code
@@ -53,11 +55,11 @@
                         return super.fen; // from <chessmeister-board>
                     }
                     set fen(value) {
-                        super.fen = value;
-                        // ChessMeister was developed a bit crude (in 2020) Should have used more Events
-                        // FEN can be set as property when board is NOT in the DOM yet
-                        // make sure board and pieces exist in the DOM then disable ChessMeister CSS & interactions
                         setTimeout(() => {
+                            super.fen = value;
+                            // ChessMeister was developed a bit crude (in 2020) Should have used more Events
+                            // FEN can be set as property when board is NOT in the DOM yet
+                            // make sure board and pieces exist in the DOM then disable ChessMeister CSS & interactions
                             // always remove the colors <chessmeister-board> puts on Chess Pieces (indicating attack/defend states)
                             const boardPieces = this.shadowRoot.querySelectorAll(`img[is],[piece="none"]`);
                             boardPieces.forEach(piece => {
@@ -68,16 +70,23 @@
                     }
                     // ==================================================== showletter decoded on board
                     showletter() {
-                        const blackPiecesAndProtectedSquares = this.shadowRoot.querySelectorAll(`[is*="black-"],[protectorsblack]`);
-                        // this.$$log("showletter", this.letter, blackPiecesAndProtectedSquares);
+                        setTimeout(() => { // make sure board is completely initiated
+                        // preferable the Web Component should emit an Event for this
+                        // but we are dealing with a Web Component from 2020 here
+
                         // all back pieces and all squares defended by black pieces
-                        blackPiecesAndProtectedSquares.forEach(square =>
-                            square.style.backgroundColor = "darkred");
+                            const selector = `[is*="black-"],[protectorsblack]`;
+                            const blackPiecesAndProtectedSquares = this.shadowRoot.querySelectorAll(selector);
+                            blackPiecesAndProtectedSquares.forEach(square =>
+                                square.style.backgroundColor = "darkred");
+                        })
                     }
-                });
-        })
-        // ********************************************************************  defined <chessmeister-board-letter>
-    });
+                }); // <chess-board-letter>
+            // ********************************************************************  defined <chessmeister-board-letter>
+
+        }) // whenDefined <chessmeister-board>
+
+    }); // whenDefined <cipher-baseclass>
 
     console.log(`%c Loaded %c ${fileName} `, "font-size:75%", "background:blue", "")
     // watch my BlueSky profile for news of my upcoming Web Component course
